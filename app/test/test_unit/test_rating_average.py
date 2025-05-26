214043f565559a1cc1a3ff90ba0c4e7384f3d0a7
Sofia Raggi <sraggi@alu.frlp.utn.edu.ar>
2025-05-26 18:41:21 -0300

    incorporacion de TestCase en 'test_rating_average.py'

--- app/test/test_unit/test_rating_average.py
+++ app/test/test_unit/test_rating_average.py
@@ -1,24 +1,29 @@
 from django.contrib.auth.models import User
+from django.test import TestCase
 from app.models import Event, Rating
 
-def test_rating_average_calculation():
-    organizer = User.objects.create_user(username="organizer", password="pass")
-    event = Event.objects.create(title="Evento Test", organizer=organizer)
 
-    Rating.objects.create(
-        evento=event, usuario=organizer,
-        titulo="Muy Bueno", calificacion=5, texto="El evento estuvo muy interesante"
-    )
+class RatingAverageCalculation(TestCase):
 
-    Rating.objects.create(
-        evento=event, usuario=User.objects.create_user(username="user2", password="pass"),
-        titulo="No me intereso", calificacion=3, texto="El evento estuvo bien, pero puede mejorar"
-    )
+    def setUp(self):
+        self.organizer = User.objects.create_user(username="organizer", password="pass")
+        self.event = Event.objects.create(title="Evento Test", organizer=self.organizer)
 
-    Rating.objects.create(
-        evento=event, usuario=User.objects.create_user(username="user3", password="pass"),
-        titulo="Malo", calificacion=1, texto="No me gusto nada"
-    )
+    def test_rating_average_calculation(self):
+        Rating.objects.create(
+            evento=self.event, usuario=self.organizer,
+            titulo="Muy Bueno", calificacion=5, texto="El evento estuvo muy interesante"
+        )
 
-    average = event.rating_average
-    assert round(average, 1) == 3.0
+        Rating.objects.create(
+            evento=self.event, usuario=User.objects.create_user(username="user2", password="pass"),
+            titulo="No me intereso", calificacion=3, texto="El evento estuvo bien, pero puede mejorar"
+        )
+
+        Rating.objects.create(
+            evento=self.event, usuario=User.objects.create_user(username="user3", password="pass"),
+            titulo="Malo", calificacion=1, texto="No me gusto nada"
+        )
+
+        average = self.event.rating_average
+        self.assertEqual(round(average, 1), 3.0)
