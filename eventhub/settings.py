820a7cf8c17391cdeccfc5bf2a266378f3ef9f6b
RomeroSilvia <romerosilvia072@gmail.com>
2025-06-09 21:53:10 -0300

    Agregar variable de entorno al archivo settings

--- eventhub/settings.py
+++ eventhub/settings.py
@@ -28,6 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',')
 
 # Application definition
 
