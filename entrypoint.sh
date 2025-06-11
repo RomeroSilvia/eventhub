4caf09fea526064915d4c86d38584a972d44509f
Sofia Goszko <sofiagoszko@gmail.com>
2025-06-10 22:11:01 -0300

    se agregaron valores por defecto a las variables de entorno

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-
 echo "Ejecutando migraciones..."
+
 python manage.py makemigrations
 python manage.py migrate
 python manage.py migrate cities_light
 python manage.py loaddata fixtures/*.json
 
 echo "Iniciando servidor Gunicorn..."
+
 exec "$@"
--- eventhub/settings.py
+++ eventhub/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
-CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,0.0.0.0').split(',')
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', 'http://localhost,http://127.0.0.1,http://0.0.0.0').split(',')
 
 # Application definition
 
